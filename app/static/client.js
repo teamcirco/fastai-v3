0de90c24e14ff3e66aae08023c07544f583704b5
teamcirco <39929830+teamcirco@users.noreply.github.com>
2020-04-14 18:47:27 -0400

    Update client.js

--- app/static/client.js
+++ app/static/client.js
@@ -30,8 +30,6 @@ function analyze() {
     if (this.readyState === 4) {
       var response = JSON.parse(e.target.responseText);
       el("result-label").innerHTML = `Result = ${response["result"]}`;
-      #el("probability-label").innerHTML = `Probability = ${response["probability"]}`;
-      
     }
     el("analyze-button").innerHTML = "Analyze";
   };
